429b0d8f2ad3b6c085d79fb5a0c1ec6f6d288deb
gaozhiwei <gaozhiwei@uniontech.com>
2022-07-12 11:16:36 +0800

    feat: add icon

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle(QStringLiteral("Deb Tools"));
+    w.setWindowIcon(QIcon("://deb_tools.svg"));
     w.show();
 
     return a.exec();
